2bd6c06ef4ef4c7ba8ce84a39d1affdec2b49663
Induja Vijayaragavan <induja.vijayaragavan@mutualofomaha.com>
2016-08-22 21:38:46 +0000

    Rspec tests added

--- spec/classes/init_spec.rb
+++ spec/classes/init_spec.rb
@@ -2,5 +2,27 @@ require 'spec_helper'
 describe 'consul_watch' do
   context 'with default values for all parameters' do
     it { should contain_class('consul_watch') }
-  end
+    it { should compile}
+    it do
+         should contain_file('/opt/continuous_delivery/bin/my-key-handler.sh').with(
+           :mode   => '0755',
+           :owner  => 'root',
+           :group  => 'root',)
+      end
+      it do
+         should contain_file('/opt/continuous_delivery/bin/consulwatch.sh').with(
+           :mode    => '0755',
+           :owner   => 'root',
+           :group   => 'root',)
+       end
+       it do
+         should contain_service('consulwatch').with(
+           :'ensure'     => 'running',
+           :'provider'   => 'base',
+           :'start'      => '/opt/continuous_delivery/bin/consulwatch.sh',
+           :'pattern'    => 'consul watch',
+           :'hasstatus'  => 'true',
+           :'hasrestart' => 'true',)
+        end
+    end
 end
